d214244b6358813f64327d08977752c47408736c
DahamyaSilva <dahamyapsilva@gmail.com>
2025-06-08 09:00:10 +0530

    added code to create a group to learn about the scene hierarchy

--- learning-three-js/src/TransformPosition.js
+++ learning-three-js/src/TransformPosition.js
@@ -7,11 +7,32 @@ const scene = new THREE.Scene();
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 const cubeMaterial = new THREE.MeshBasicMaterial({color: "cyan"});
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
-scene.add(cubeMesh);
+cubeMesh.position.y = -1;
+
+//adding more meshes
+const cubeMesh2 = new THREE.Mesh(cubeGeometry, cubeMaterial);
+cubeMesh2.position.x = 2;
+const cubeMesh3 = new THREE.Mesh(cubeGeometry, cubeMaterial);
+cubeMesh3.position.x = -2;
+
+//creating a group
+const  group = new THREE.Group();
+group.add(cubeMesh);
+group.add(cubeMesh2);
+group.add(cubeMesh3);
+
+group.position.y = 2;
+
+scene.add(group);
+
+// scene.add(cubeMesh);
+
+//adding scales to change the scale of the object
+cubeMesh.scale.set(1,1,1);
 
 //vector 3
-const tempVector = new THREE.Vector3(0, 1, 0);
-cubeMesh.position.copy(tempVector);
+// const tempVector = new THREE.Vector3(0, 0, 0);
+// cubeMesh.position.copy(tempVector);
 
 //added axes helper
 const axesHelper = new THREE.AxesHelper(2);
